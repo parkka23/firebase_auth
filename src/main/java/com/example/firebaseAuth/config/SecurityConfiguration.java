2f25243323db8033eb8ed645c90b3f058d508608
ksenia <park.ksenia23@gmail.com>
2023-08-09 22:38:13 +0600

    commit

--- src/main/java/com/example/firebaseAuth/config/SecurityConfiguration.java
+++ src/main/java/com/example/firebaseAuth/config/SecurityConfiguration.java
@@ -2,25 +2,60 @@ package com.example.firebaseAuth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+
+//
+//@Configuration
+//public class SecurityConfiguration {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/app/register").permitAll()// Allow unauthenticated access to /app/register
+//                .anyRequest().authenticated();
+//
+//        http.oauth2ResourceServer().jwt();
+//
+//        return http.build();
+//    }
+//
+//
+//}
 
 @Configuration
 public class SecurityConfiguration {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/app/register").permitAll()// Allow unauthenticated access to /app/register
-                .anyRequest().authenticated();
-
-        http.oauth2ResourceServer().jwt();
-
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/app/register").permitAll()
+//                                .antMatchers("/app/test").anonymous()
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2
+                                .jwt(jwt ->
+                                        jwt.decoder(jwtDecoder())
+                                )
+                );
         return http.build();
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromOidcIssuerLocation("https://securetoken.google.com/spring-firebase-auth");
+    }
 }
--- src/main/java/com/example/firebaseAuth/controller/AppController.java
+++ src/main/java/com/example/firebaseAuth/controller/AppController.java
@@ -4,10 +4,16 @@ package com.example.firebaseAuth.controller;
 import com.example.firebaseAuth.entity.UserAuth;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import com.google.firebase.auth.UserRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -15,25 +21,36 @@ import java.security.Principal;
 @RestController
 @RequestMapping("/app")
 public class AppController {
+    private final FirebaseAuth firebaseAuth;
+
+    @Autowired
+    public AppController(FirebaseAuth firebaseAuth) {
+        this.firebaseAuth = firebaseAuth;
+    }
+
 
 //    @GetMapping(path = "/test")
-//    public String test(Principal principal) {
-//        return principal.getName();
+//    public String test() {
+//        UserRecord userRecord = FirebaseAuth.getInstance().getUser(uid);
+//        return "Hello";
 //    }
 
 
-
     @GetMapping(path = "/test")
-    public String test() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            // The user is authenticated, get the displayName
-            String displayName = authentication.getName();
-            return "Hello, " + displayName;
-        } else {
-            return "Hello, Unauthenticated User";
+    public String test(@RequestHeader("Authorization") String authorizationHeader) throws FirebaseAuthException {
+        String idToken = authorizationHeader.replace("Bearer ", "");
+        try {
+            FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
+            String uid = decodedToken.getUid();
+            UserRecord userRecord = FirebaseAuth.getInstance().getUser(uid);
+            return "Hello, "+userRecord.getDisplayName()+"! ^_^";
+        }
+        catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            return "Token spelled wrong or expired.";
         }
     }
+
 
     @PostMapping(path = "/register")
     public String registerUser(@RequestBody UserAuth userAuth) {
